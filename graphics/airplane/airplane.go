5e80d1a521b5a65fd489f7857662dde72177735d
davidreis97 <david.alex.reis.97@gmail.com>
2020-08-08 22:50:54 +0100

    Fix ambient component in airplane material
    obj.Decoder sets all ambient component to 0 by default.
    Fixed by using the diffuse component value as the ambient component.

--- graphics/airplane/airplane.go
+++ graphics/airplane/airplane.go
@@ -7,11 +7,21 @@ import (
 
 // Init loads the spaceship .obj file
 func Init() (*graphic.Mesh, error) {
-	decoder, err := obj.Decode("assets/spaceModels/spaceCraft1.obj", "assets/spaceModels/spaceCraft1.mtl")
+	decoder, err := obj.Decode("assets/spaceModels/spaceCraft1.obj", "")
 
 	if err != nil {
 		return nil, err
 	}
-	
-	return decoder.NewMesh(&decoder.Objects[0])
+
+	for _, mat := range decoder.Materials {
+		mat.Ambient = mat.Diffuse
+	}
+
+	mesh, err := decoder.NewMesh(&decoder.Objects[0])
+
+	if err != nil {
+		return nil, err
+	}
+
+	return mesh, err
 }
